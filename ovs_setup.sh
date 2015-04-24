f514ae20990bd374b945afaee33f98e92d9e604d
Peter Bryzgalov <petr.bryzgalov@riken.jp>
2015-04-24 11:50:55 +0900

    Modified ovs_setup.sh for creating setups for remote benchmarking through OVS bridge

--- ovs_setup.sh
+++ ovs_setup.sh
@@ -1,37 +1,66 @@
 #!/bin/bash
 
-# Setup OVS connections from two containers to ovs-bridge interface on host machine
-
-if [ $# -gt 1 ]
-then
-    printf 'Setup OVS connections between two containers via ovs-bridge (must exist!) on host machine\nContainers will be named iperfserv and iperfclient with IP 10.0.0.5 and 10.0.0.6, created from image peter/iperf.\nNeed no parameters.\n'
-    exit 0
-fi
+# Setup OVS connections from containers to ovs-bridge interface on host machine
+# and setup tunnel from ovs-bridge to remote host with given IP.
+# Takes three parametes: container IP with net mask, remote IP and OVS tunnel type
 
 set -e
 
-clnt=$(docker run -name iperfclient -d peter/iperf /usr/sbin/sshd -D)
-serv=$(docker run -name iperfserv -d peter/iperf /usr/sbin/sshd -D)
+echo "v0.5"
 
-echo "Created containers iperfserv $serv and iperfclient $clnt"
+usage=$(cat <<USAGE
+Setup OVS connection from a container to ovs-bridge on host machine
+and setup tunnel from ovs-bridge to remote host with given IP.
+Parameters: container IP, remote host IP and OVS tunnel type.\n
+USAGE
+)
 
-clntPID=$(docker inspect iperfclient | jq '.[0].State.Pid')
-servPID=$(docker inspect iperfserv | jq '.[0].State.Pid')
-
-if [ ! -d /var/run/netns/ ]
-then 
-    mkdir -p /var/run/netns/
+# Check that ovs_connect_container.sh exists
+fname="ovs_connect_container.sh"
+if [ ! -x "$fname" ]
+then
+    echo "Need executable $fname in the same directory."
+    exit 1
 fi
 
-ln -s /proc/$clntPID/ns/net /var/run/netns/$clntPID 
-ln -s /proc/$servPID/ns/net /var/run/netns/$servPID
+if [ $# -lt 2 ]
+then
+    echo "$usage"
+    exit 0
+fi
 
-ovs-vsctl add-port ovs-bridge tap1 -- set Interface tap1 type=internal
-ovs-vsctl add-port ovs-bridge tap2 -- set Interface tap2 type=internal
-ip link set tap1 netns $clntPID
-ip link set tap2 netns $servPID
+CntIP=$1
+# Check if a subnet mask was provided.
+echo $CntIP | grep -q / || {
+    echo "The IP address must include a netmask."
+    echo "Maybe you meant $CntIP/24 ?"
+    exit 1
+}
+RmtIP=$2
+TT="gre"
+if [ $3 ]
+then
+    TT="$3"
+fi
 
-ip netns exec $clntPID ip link set dev tap1 up
-ip netns exec $servPID ip link set dev tap2 up
-ip netns exec $clntPID ip addr add dev tap1 10.0.0.5/24
-ip netns exec $servPID ip addr add dev tap2 10.0.0.6/24
+# Create bridge
+bridge="ovs-bridge"
+# Check if bridge existst
+if [ -z "$(ip a s | grep $bridge)" ]
+then
+    ovs-vsctl add-br $bridge
+fi
+
+# Add port to bridge, create interface
+port="ovs1"
+echo "Setting bridge $bridge with tunnel $port type $TT to $RmtIP."
+ovs-vsctl add-port $bridge $port -- set interface $port type=$TT options:remote_ip=$RmtIP
+
+# Start container and connect it to the bridge
+cont=$(docker run --name iperf_$TT -d peter/iperf /usr/sbin/sshd -D)
+echo "Started container $cont \"iperf_$TT\"."
+
+./ovs_connect_container.sh $cont $CntIP $bridge
+
+echo "Setup complete"
+
